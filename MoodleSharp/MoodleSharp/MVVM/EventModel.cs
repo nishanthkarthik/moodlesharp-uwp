7b434a1107d2b18570eb73c8ecb666158072ed6b
Karthik Nishanth Sengottuvelavan <nishanthkarthik@live.com>
2015-08-01 22:51:10 +0530

    Initial Project commit

---
+++ MoodleSharp/MoodleSharp/MVVM/EventModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MoodleSharp.MVVM
+{
+    class EventCategory
+    {
+        public string Title { get; set; }
+        public List<Event> Events { get; set; }
+    }
+
+    class Event
+    {
+        [JsonProperty]
+        public string Title { get; set; }
+        [JsonProperty]
+        public string ImageUri { get; set; }
+        [JsonProperty]
+        public string Subtitle { get; set; }
+        [JsonProperty]
+        public string Category { get; set; }
+        [JsonProperty]
+        public string Description { get; set; }
+    }
+}
---
+++ MoodleSharp/MoodleSharp/MVVM/EventViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Newtonsoft.Json;
+
+namespace MoodleSharp.MVVM
+{
+    class EventViewModel
+    {
+        List<EventCategory> Items { get; set; }
+
+        private EventViewModel() { }
+
+        public async Task<EventViewModel> InitializeAsync()
+        {
+            try
+            {
+                IStorageFile storageFile = await Package.Current.InstalledLocation.GetFileAsync(@"MVVM\MOCK_DATA.json");
+                string jsonString = await FileIO.ReadTextAsync(storageFile);
+                List<Event> events = JsonConvert.DeserializeObject<List<Event>>(jsonString);
+                Items = events.GroupBy(x => x.Category)
+                    .Select(x => new EventCategory() { Title = x.Key, Events = x.ToList() }).ToList();
+                return this;
+            }
+            catch (Exception exception)
+            {
+                await new MessageDialog(exception.Message).ShowAsync();
+                throw;
+            }
+        }
+
+        public static Task<EventViewModel> CreateTaskAsync()
+        {
+            EventViewModel eventViewModel = new EventViewModel();
+            return eventViewModel.InitializeAsync();
+        }
+    }
+}
